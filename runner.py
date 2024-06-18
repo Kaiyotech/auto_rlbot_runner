790e4e1a266453b62ff994f1ab6cd4a1daa23261
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2024-06-17 21:44:50 -0400

    add pinch

--- runner.py
+++ runner.py
@@ -379,6 +379,8 @@ def get_opponent(blue=False):
                 return [get_bot_config_bundle("C:\\Users\\kchin\\Code\\Kaiyotech\\opti_play_redis\\bot_db.cfg")]
             elif line.lower() == 'opti-dt' or line.lower() == 'opti_dt':
                 return [get_bot_config_bundle("C:\\Users\\kchin\\Code\\Kaiyotech\\opti_play_redis\\bot_dt.cfg")]
+            elif line.lower() == 'opti-dt' or line.lower() == 'opti_pinch':
+                return [get_bot_config_bundle("C:\\Users\\kchin\\Code\\Kaiyotech\\opti_play_redis\\bot_pinch.cfg")]
             elif line.lower() == 'necto':
                 bot_bundle = list(scan_directory_for_bot_configs(
              "C:\\Users\\kchin\\AppData\\Local\\RLBotGUIX\\RLBotPackDeletable\\RLBotPack-master\\RLBotPack\\Necto\\Necto"))
